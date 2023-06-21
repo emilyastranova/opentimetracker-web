4660ec7317538e98cd79dd0a33c627b4943d1923
Tyler Harrison <tyleraharrison@gmail.com>
2023-06-20 22:21:06 -0500

    Basic styling and layout done
    Signed-off-by: Tyler Harrison <tyleraharrison@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import Fa from 'svelte-fa';
+	import { faClock } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div class="relative px-8">
+	<Navbar
+		navClass="px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0 border-b"
+		let:hidden
+		let:toggle
+	>
+		<NavBrand href="/">
+			<Fa icon={faClock} class="text-xl mr-2" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+				>OpenTimeTracker</span
+			>
+		</NavBrand>
+		<NavHamburger on:click={toggle} />
+		<NavUl {hidden}>
+			<NavLi href="/" active={true}>Home</NavLi>
+			<NavLi href="/card">Time Card</NavLi>
+			<NavLi href="/sheets">Time Sheets</NavLi>
+			<NavLi href="/settings">Settings</NavLi>
+		</NavUl>
+	</Navbar>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+
+	plugins: [require('flowbite/plugin')],
+
+	darkMode: 'media',
+
+	theme: {
+		extend: {
+			colors: {
+				// flowbite-svelte
+				primary: {
+					50: '#E3F2FD',
+					100: '#BBDEFB',
+					200: '#90CAF9',
+					300: '#64B5F6',
+					400: '#42A5F5',
+					500: '#2196F3',
+					600: '#1E88E5',
+					700: '#1976D2',
+					800: '#1565C0',
+					900: '#0D47A1'
+				}
+			},
+			fontFamily: {
+				sans: [
+					'Inter',
+					'ui-sans-serif',
+					'system-ui',
+					'-apple-system',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji'
+				]
+			}
+		}
+	}
+};
